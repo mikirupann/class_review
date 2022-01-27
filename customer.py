6c94d47f39bf0ab292dc5d4dceb84732bf40ecf2
mikirupann <sw.miki.0907@icloud.com>
2022-01-27 17:23:37 +0900

    C-8まで完了
    よろしくおねがいします

--- customer.py
+++ customer.py
@@ -9,7 +9,7 @@ class Customer:
 
     def entry_free(self):
         if self.age <= 3:
-            return free
+            return 0
         elif self.age < 20:
             return 1000
         elif 20 <= self.age < 65:
@@ -22,6 +22,11 @@ class Customer:
     def info_csv(self):
         return f"\"{self.first_name},{self.family_name},{self.age},{self.entry_free()}\""
 
+    def info_tab(self):
+        return f"{self.first_name} {self.family_name} {self.age} {self.entry_free()}"
+
+    def info_pipe(self):
+        return f"{self.first_name} {self.family_name}|{self.age}|{self.entry_free()}"
 ken = Customer(first_name='Ken', family_name='Tanaka', age=15)
 tom = Customer(first_name='Tom', family_name='Ford', age=57)
 ieyasu = Customer(first_name='Ieyasu', family_name='Tokugawa', age=73)
@@ -51,3 +56,12 @@ print()
 
 # C-5.C-6
 # C-7
+print(ken.info_tab())
+print(tom.info_tab())
+print(ieyasu.info_tab())
+print()
+
+print(ken.info_pipe())
+print(tom.info_pipe())
+print(ieyasu.info_pipe())
+print()
